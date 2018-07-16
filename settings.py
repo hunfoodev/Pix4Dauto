794ff849ef4a73ce346210947164d59869513fe4
Zachary Deziel <zachary.deziel@usherbrooke.ca>
2018-07-16 12:11:28 -0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+# base directory of the mission
+baseDir = 'C:/Users/zdeziel/Documents/TELUQ/missions/thetford-20180713/'
+
+# Pix4D template used for your projects
+# A template sets all settings of a project
+template = 'TELUQgeneral'
---
+++ test.py
@@ -0,0 +1,5 @@
+from pix4dAutoFonctions import write_pix4dBat
+from settings import baseDir, template
+
+# write bash file
+write_pix4dBat(baseDir, template)
